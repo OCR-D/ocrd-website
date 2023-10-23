dfcf8c6a39c32d0e03731717a4ff20e5c6a91fab
Konstantin Baierer <unixprog@gmail.com>
2023-10-23 15:39:07 +0200

    update core docs

--- site/core/_static/documentation_options.js
+++ site/core/_static/documentation_options.js
@@ -1,7 +1,7 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '2.41.0',
-    LANGUAGE: 'None',
+    VERSION: '2.58.1',
+    LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
     FILE_SUFFIX: '.html',
